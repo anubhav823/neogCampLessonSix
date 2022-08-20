496459db80967d291db7513e1e1814933c3e0edd
Anubhav Jain <anubhav823@gmail.com>
2022-08-20 14:55:59 +0530

    added validation display logic change and styling

--- script.js
+++ script.js
@@ -4,47 +4,52 @@ const button = document.querySelector('#get_number_of_notes')
 const validations = document.querySelector('#validations')
 const notes = document.querySelectorAll('.quantities');
 const notesValue = [2000, 500, 100, 50, 10, 1];
-
+validations.style.display = 'none'
 button.addEventListener('click', validateAmounts);
 
-function validateAmounts(){
-    validations.innerHTML ='';
+function validateAmounts() {
+    validations.innerHTML = '';
     billAmountValidator();
     cashAmountValidator();
 }
-function billAmountValidator(){
-    if(isNaN(bill_amount_input.value)){
+function billAmountValidator() {
+    let validationMessage = document.createElement('p');
+    if (isNaN(bill_amount_input.value)) {
         validationMessage.innerText = 'Bill Amount should be a number'
-    }
-    if(bill_amount_input.value > 0){
+        validations.appendChild(validationMessage)
+        validations.style.display = 'block'
+    } else if (bill_amount_input.value > 0) {
         console.log('bill_amount_input.value = ' + bill_amount_input.value);
     } else {
         let validationMessage = document.createElement('p');
-        validationMessage.innerText = 'please enter a number larger than zero'
-        validations.appendChildv(validationMessage)
+        validationMessage.innerText = 'Please enter a number larger than zero'
+        validations.appendChild(validationMessage)
+        validations.style.display = 'block'
     }
 }
 
-function cashAmountValidator(){
+function cashAmountValidator() {
     let validationMessage = document.createElement('p');
-    if(isNaN(cash_given_input.value)){
+    if (isNaN(cash_given_input.value)) {
         validationMessage.innerText = 'Cash amount should be a number'
         validations.appendChild(validationMessage)
+        validations.style.display = 'block'
     }
-    if(cash_given_input.value > 0 && cash_given_input.value >= bill_amount_input.value){
+    else if (cash_given_input.value > 0 && cash_given_input.value >= bill_amount_input.value) {
         console.log('cash_amount_input.value = ' + cash_given_input.value);
         notesNeededCalculator();
     } else {
         let validationMessage = document.createElement('p');
         validationMessage.innerText = 'Enter a number larger than the bill amount'
         validations.appendChild(validationMessage)
+        validations.style.display = 'block'
     }
 }
 
-function notesNeededCalculator(){
+function notesNeededCalculator() {
     let amountToReturn = cash_given_input.value - bill_amount_input.value;
-    for(let i = 0;i< notesValue.length;i++){
-        notes[i].innerText = Math.trunc(amountToReturn/notesValue[i]);
-        amountToReturn = amountToReturn%notesValue[i];
+    for (let i = 0; i < notesValue.length; i++) {
+        notes[i].innerText = Math.trunc(amountToReturn / notesValue[i]);
+        amountToReturn = amountToReturn % notesValue[i];
     }
 }
